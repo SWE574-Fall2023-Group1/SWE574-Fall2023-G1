a3c38961f366c23d8f4c054f4f3ff54e7ed332d6
Oguz The Senna <93736051+oguzsenna@users.noreply.github.com>
2023-10-23 22:19:18 +0300

    Display notification if signup is ok  #42

--- backend/frontend/src/pages/landing/Register.js
+++ backend/frontend/src/pages/landing/Register.js
@@ -15,65 +15,65 @@ function Register() {
 
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
     if (password !== passwordAgain) {
       toast.error("Passwords don't match");
       return;
     }
-    axios.post(`http://${process.env.REACT_APP_BACKEND_HOST_NAME}:8000/user/register`, {
-      username: username,
-      email: email,
-      password: password,
-      password_again: passwordAgain
-    }).then(response => {
-      toast.success(response.data.message);
-      navigate('/login');
-    }).catch(error => {
-      if (error.response && error.response.data) {
-        if (error.response.data.username) {
-          toast.error(`Username: ${error.response.data.username}`);
-        }
-        if (error.response.data.email) {
-          toast.error(`Email: ${error.response.data.email}`);
-        }
+
+    try {
+      const response = await axios.post(`http://${process.env.REACT_APP_BACKEND_HOST_NAME}:8000/user/register`, {
+        username: username,
+        email: email,
+        password: password,
+        password_again: passwordAgain
+      });
+
+      if (response.data.success) {
+        toast.success(response.data.message);
+        setTimeout(() => {
+          navigate('/login');
+        }, 1500);
       } else {
-        toast.error('An error occurred while registering');
+        if (response.data.errors) {
+          if (response.data.errors.username) {
+            toast.error(`Username: ${response.data.errors.username}`);
+          }
+          if (response.data.errors.email) {
+            toast.error(`Email: ${response.data.errors.email}`);
+          }
+        } else {
+          toast.error('An error occurred while registering');
+        }
       }
-    });
+    } catch (error) {
+      console.log(error);
+      toast.error('An error occurred while registering');
+    }
   }
 
   return (
     <>
-      {/* <form onSubmit={handleSubmit}> */}
-        <h1>Register</h1>
-        <div>
-          {/* <label>Username:</label>
-          <input type="text" className="form-control" onChange={(e) => setUsername(e.target.value)} /> */}
-          <TextField id="outlined-basic" label="Username" variant="outlined" onChange={(e) => setUsername(e.target.value)} />
-        </div>
-        <br/>
-        <div>
-          {/* <label>Email:</label>
-          <input type="email" className="form-control" onChange={(e) => setEmail(e.target.value)} /> */}
-          <TextField id="outlined-basic" label="E-Mail" variant="outlined" onChange={(e) => setEmail(e.target.value)} />
-        </div>
-        <br/>
-        <div>
-          {/* <label>Password:</label>
-          <input type="password" className="form-control" onChange={(e) => setPassword(e.target.value)} /> */}
-          <TextField id="outlined-basic" label="Password" variant="outlined" type='password' onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <br/>
-        <div>
-          {/* <label>Password Again:</label>
-          <input type="password" className="form-control" onChange={(e) => setPasswordAgain(e.target.value)} /> */}
-          <TextField id="outlined-basic" label="Password Again" variant="outlined" type='password' onChange={(e) => setPasswordAgain(e.target.value)} />
-        </div>
-        <br/>
-        {/* <button type="submit" className="btn btn-primary">Register</button> */}
-        <Button variant="contained" onClick={handleSubmit} className="register-button" >Register</Button>
-      {/* </form> */}
+      <h1>Register</h1>
+      <div>
+        <TextField id="outlined-basic" label="Username" variant="outlined" onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      <br/>
+      <div>
+        <TextField id="outlined-basic" label="E-Mail" variant="outlined" onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <br/>
+      <div>
+        <TextField id="outlined-basic" label="Password" variant="outlined" type='password' onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <br/>
+      <div>
+        <TextField id="outlined-basic" label="Password Again" variant="outlined" type='password' onChange={(e) => setPasswordAgain(e.target.value)} />
+      </div>
+      <br/>
+      <Button variant="contained" onClick={handleSubmit} className="register-button" >Register</Button>
       <ToastContainer />
     </>
   );
