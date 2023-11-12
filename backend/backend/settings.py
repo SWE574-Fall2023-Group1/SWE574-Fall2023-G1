6cceb567ae54eb28f65c967d4ecbae01c53e094d
SADIK KUZU <sadikkuzu@hotmail.com>
2023-11-12 14:44:11 +0300

    Clear Django STATICFILES_DIRS list #122

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -142,9 +142,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend', 'dist'),
-]
+STATICFILES_DIRS = []
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
