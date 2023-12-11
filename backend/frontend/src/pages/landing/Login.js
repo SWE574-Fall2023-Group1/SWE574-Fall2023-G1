f598c8d9214490ebbc94cd396aab4df4cc9169c8
Deniz Aslan <d_b_aslan@windowslive.com>
2023-12-11 18:36:30 +0300

    Fix login screen scaling and logo position

--- backend/frontend/src/pages/landing/Login.js
+++ backend/frontend/src/pages/landing/Login.js
@@ -54,47 +54,17 @@ function Login({ onLoginSuccess }) {
   };
 
   return (
-    <div className="container">
+    <div className="container" style={{display: "flex", "flex-direction": "column", "justify-content": "center", "align-items": "center", "text-align": "center", margin: "auto"}}>
       {/* <form onSubmit={handleSubmit}> */}
-      <div style={{width: '100%', height: '100%', position: 'relative'}}>
-        <div style={{width: 900, height: 1005, left: 550, top: 0, position: 'absolute', background: 'white', borderTopLeftRadius: 40, borderTopRightRadius: 40, overflow: 'hidden'}}>
-            <div style={{width: 339, height: 53, left: 304, top: 140, position: 'absolute', color: '#2C2A2A', fontSize: 15, fontFamily: "'Josefin Sans', sans-serif", fontWeight: '700', wordWrap: 'break-word'}}>
-            <h1>Every Memory Counts:</h1>
+      <div style={{display: "flex", "flex-direction": "column", "justify-content": "center", "align-items": "center", "text-align": "center", margin: "auto", width: '100%', height: '100%', "flex-wrap": "wrap"}}>
+      <br/>
+        <div style={{display: "flex", "flex-direction": "column", "justify-content": "center", "align-items": "center", "text-align": "center", margin: "auto", width: "80%", background: 'white', borderRadius: 40, "flex-wrap": "wrap"}}>
+        <br/>
+        <img style={{display: "flex", "flex-direction": "column", "justify-content": "center", "align-items": "center", "text-align": "center", width: 449}} src={mainPhoto} />
+            <h1>Every Memory Counts.</h1>
             <h2>Write, Share, Relive.</h2>
-            </div>
                  {/* <button type="submit" className="btn btn-primary">Login</button> */}
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handleSubmit}
-                  className="login-button"
-                  style={{
-                    width: 473,
-                    height: 53,
-                    paddingLeft: 40,
-                    paddingRight: 40,
-                    paddingTop: 15,
-                    paddingBottom: 15,
-                    left: 312,
-                    top: 430,
-                    position: 'absolute',
-                    borderRadius: 8,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    gap: 10,
-                    display: 'inline-flex',
-                    cursor: 'pointer',
-                    fontSize: 16,
-                    fontFamily: "'Josefin Sans', sans-serif",
-                    fontWeight: '700',
-                    wordWrap: 'break-word',
-                    textTransform: 'none',
-                    backgroundColor: 'purple',
-                  }}
-                >
-                  Login
-              </Button>
-              <div style={{ width: 473, height: 59, left: 304, top: 240, position: 'absolute', background: 'white', borderRadius: 8, padding: 8 }}>
+              <div style={{display: "flex", "flex-direction": "column", "justify-content": "center", "align-items": "center", "text-align": "center", width: 473, height: 59, background: 'white', borderRadius: 8, padding: 8 }}>
                 {/* <label>Username:</label>
                   <input type="text" className="form-control" onChange={(e) => setUsername(e.target.value)} /> */}
                 <TextField
@@ -115,7 +85,7 @@ function Login({ onLoginSuccess }) {
                     }}
                 />
             </div>
-            <div style={{width: 473, height: 59, left: 304, top: 320, position: 'absolute', background: 'white', borderRadius: 8, padding: 8}}>
+            <div style={{display: "flex", "flex-direction": "column", "justify-content": "center", "align-items": "center", "text-align": "center", margin: "auto", width: 473, height: 59,background: 'white', borderRadius: 8, padding: 8}}>
                     <TextField
                             id="login-password"
                             label="Password"
@@ -134,13 +104,44 @@ function Login({ onLoginSuccess }) {
                                 boxSizing: 'border-box', // Include padding and borders in the total width and height
                             }}
                     />
-                </div>
-                <div style={{left: 307, top: 492, position: 'absolute'}}><span style={{color: '#7C7A7A', fontSize: 16, fontFamily: "'Josefin Sans', sans-serif", fontWeight: '500', wordWrap: 'break-word' }} >Don't you have any account yet? </span><button onClick= {navigatetoforgotpassword} style= {{color: '#AF49FF', fontSize: 16, fontFamily: "'Josefin Sans', sans-serif", fontWeight: '500', wordWrap: 'break-word', cursor: 'pointer', border: 'none', background:'none'}} >Register Now!</button></div>
-                <div style={{left: 307, top: 400, position: 'absolute'}}><span style={{color: '#2C2A2A', fontSize: 16, fontFamily: "'Josefin Sans', sans-serif", fontWeight: '500', wordWrap: 'break-word' }} ></span><button onClick= {navigatetoregister} style= {{color: '#AF49FF', fontSize: 16, fontFamily: "'Josefin Sans', sans-serif", fontWeight: '500', wordWrap: 'break-word', cursor: 'pointer', border: 'none', background:'none'}} >Forgot password?</button></div>
         </div>
-        <img style={{width: 449, height: 400, left: 310, top: 200, position: 'absolute'}} src={mainPhoto} />
+        <br/>
+        <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleSubmit}
+                  className="login-button"
+                  style={{
+                    width: 473,
+                    height: 53,
+                    paddingLeft: 40,
+                    paddingRight: 40,
+                    paddingTop: 15,
+                    paddingBottom: 15,
+                    borderRadius: 8,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    gap: 10,
+                    display: 'inline-flex',
+                    cursor: 'pointer',
+                    fontSize: 16,
+                    fontFamily: "'Josefin Sans', sans-serif",
+                    fontWeight: '700',
+                    wordWrap: 'break-word',
+                    textTransform: 'none',
+                    backgroundColor: 'purple',
+                    "margin-left": "auto",
+                    "margin-right": "auto",
+                  }}
+                >Login</Button>
+                <br/>
+                <div style={{}}><span style={{color: '#7C7A7A', fontSize: 16, fontFamily: "'Josefin Sans', sans-serif", fontWeight: '500', wordWrap: 'break-word' }} >Don't have any account yet? </span><button onClick= {navigatetoforgotpassword} style= {{color: '#AF49FF', fontSize: 16, fontFamily: "'Josefin Sans', sans-serif", fontWeight: '500', wordWrap: 'break-word', cursor: 'pointer', border: 'none', background:'none'}} >Register Now!</button></div>
+                <br/>
+                <div style={{}}><span style={{color: '#2C2A2A', fontSize: 16, fontFamily: "'Josefin Sans', sans-serif", fontWeight: '500', wordWrap: 'break-word' }} ></span><button onClick= {navigatetoregister} style= {{color: '#AF49FF', fontSize: 16, fontFamily: "'Josefin Sans', sans-serif", fontWeight: '500', wordWrap: 'break-word', cursor: 'pointer', border: 'none', background:'none'}} >Forgot password?</button></div>
+        <br/>
+        </div>
       </div>
-        <ToastContainer position="bottom-right" autoClose={5000} />
+        <ToastContainer position="bottom-right" autoClose={5000}  />
         <br/>
       {/* </form> */}
     </div>
