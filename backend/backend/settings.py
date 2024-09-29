d56bdcdfd0107c5cb42656828a430f4334041f0d
SADIK KUZU <sadikkuzu@hotmail.com>
2024-09-29 13:19:03 +0300

    Add rest_framework.authtoken again #25

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework.authtoken',
     'user',
     'rest_framework',
     'corsheaders',
